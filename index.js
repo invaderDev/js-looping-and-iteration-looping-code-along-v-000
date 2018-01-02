0a2b617cb90f7e577ca0f24c2ec9e96f9251ba5f
Kris Thompson <kt.devcat@gmail.com>
2018-01-02 18:15:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Code your solutions in this file
 function printBadges(empNames) {
   for (i = 0; i < empNames.length; i++) {
-    console.log(`Welcome ${empNames[i]}! You are employee #${i + 1}.`)
+    console.log(`Welcome ${empNames[i]}! You are employee #${i + 1}.`);
   }
+
+  return empNames;
 }
